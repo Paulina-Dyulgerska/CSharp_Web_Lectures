94a80850119583ecef73743e2dce5a772d1c343f
Paulina-Dyulgerska <paylina_st@yahoo.com>
2020-09-20 13:40:23 +0300

    Initial Upload

---
+++ PrimeNumbersCounter/PrimeNumbersCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeNumbersCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
