806f4e2b710beae15ae097f6375c97bb59ca5172
Paulina-Dyulgerska <paylina_st@yahoo.com>
2020-09-27 18:15:54 +0300

    Update with Cookies and Sessions Lecture

--- PrimeNumbersCounter/HttpServer/Program.cs
+++ PrimeNumbersCounter/HttpServer/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,7 +12,10 @@ namespace HttpServer
 {
     class Program
     {
+        static Dictionary<string, int> SessionStorage = new Dictionary<string, int>();
+
         const string NewLine = "\r\n";
+
         static async Task Main(string[] args) //method ne e void veche, a e Task!!! t.e. vryshta Task, zaradi mnogozadachnostta!
         {
             //await ReadData();
@@ -35,6 +40,9 @@ namespace HttpServer
 
         private static async Task ProcessClientAsync(TcpClient client)
         {
+            var clientSocket = client.Client.RemoteEndPoint;
+            Console.WriteLine(clientSocket); //vryshta mi tova: 127.0.0.1:65245 - tova e IP-to i porta na browsera na clienta
+
             using (var stream = client.GetStream())
             {
                 byte[] buffer = new byte[1000000];
@@ -43,16 +51,35 @@ namespace HttpServer
                 string requestString = Encoding.UTF8.GetString(buffer, 0, lenght);
                 Console.WriteLine(requestString);
 
-                bool sessionSet = false;
+                //Thread.Sleep(10000); //spiram threada za 10 secunds.
 
+                //variant da vidq dali veche imam session setnal i da spra da prashtam cookie-to za session id:
+                bool sessionSet = false;
                 if (requestString.Contains("sid"))
                 {
                     sessionSet = true;
                 }
+                var sid = Guid.NewGuid().ToString();
+                //tyrsq dali browsera mi e pratil sid v requesta:
+                var match = Regex.Match(requestString, @"sid=[^\n]*\r\n");
+                //tyrsq neshto da pochva s sid= posle da ne e nov red i da
+                //svyrshva na nov red. Ako namerq takowa neshto, towa znachi, 
+                //che imam cookie, koeto mi prashta session id-to.
+                //edin i syshti client mi prashta edno i syshto sid!!!
+                if (match.Success)
+                {
+                    sid = match.Value.Substring(4);
+                }
+                Console.WriteLine(sid);
 
-                //Thread.Sleep(10000); //spiram threada za 10 secunds.
+                //v SessionStarage syhranqwam sega prosto kolko pyti e otvarqna dadena stranica:
+                if (!SessionStorage.ContainsKey(sid))
+                {
+                    SessionStorage.Add(sid, 0);
+                }
+                SessionStorage[sid]++;
 
-                string html = $"<h1>Hello from PaulinaServer {DateTime.Now}</h1>" +
+                string html = $"<h1>Hello from PaulinaServer {DateTime.Now} for the {SessionStorage[sid]} time.</h1>" +
                     $"<form action=/tweet method=post><input name=username /><input name=password />" +
                     $"<input type=submit /></form>" + DateTime.Now;
 
@@ -60,9 +87,11 @@ namespace HttpServer
                     "Server: PaulinaServer 2020" + NewLine +
                     // "Location: https://www.google.com" + NewLine +
                     "Content-Type: text/html; charset=utf-8" + NewLine +
-                    "Set-Cookie: sid2=0; Domein=localhost; Path=/; Expires=" +
-                                            DateTime.UtcNow.AddSeconds(20).ToString("R") + NewLine +
-                    (!sessionSet ? ("Set-Cookie: sid=daeqweqwe921302932131231" + NewLine) : null) +
+                    //"Set-Cookie: sid2=0; Domein=localhost; Path=/; Expires=" + DateTime.UtcNow.AddHours(1).ToString("R") + NewLine +
+                    //"Set-Cookie: sid3=0; Domein=localhost; Path=/; Max-Age=" + (24 * 60 * 60) + "; HttpOnly" + NewLine +
+                    //"Set-Cookie: sid4=0; Domein=localhost; Path=/; Max-Age=0" + "; HttpOnly" + NewLine +
+                    //"Set-Cookie: sid5=0; Domein=localhost; Path=/; Expires=" + DateTime.Parse("01/01/2020").ToString("R") + NewLine +
+                    (!sessionSet ? ($"Set-Cookie: sid={sid}" + NewLine) : null) +
                     //tova znachi: ako ne mi e dalo session kato cookie v requesta, togawa mu prati towa
                     //cookie, no ako mi e dali session, to togawa ne mu prashtaj tozi Set-Cookie header!
                     //"Set-Cookie: sid2=2; Domein=localhost; Path=/account" + NewLine +
