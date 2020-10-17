50ebae3dc55acba66511e3a9eae1dc7ecd5da461
Paulina-Dyulgerska <paylina_st@yahoo.com>
2020-10-17 15:13:03 +0300

    Update

--- SUS/SUS.MvcFramework.Tests/SusViewEngineTests.cs
+++ SUS/SUS.MvcFramework.Tests/SusViewEngineTests.cs
@@ -26,7 +26,7 @@ namespace SUS.MvcFramework.Tests
 
             IViewEngine viewEngine = new SusViewEngine();
             var view = File.ReadAllText($"ViewTests/{fileName}.html");
-            var result = viewEngine.GetHtml(view, viewModel);
+            var result = viewEngine.GetHtml(view, viewModel, null);
             var expectedResult = File.ReadAllText($"ViewTests/{fileName}.Result.html");
             Assert.Equal(expectedResult, result);
         }
@@ -39,7 +39,7 @@ namespace SUS.MvcFramework.Tests
 {
 <span>@num</span>
 }
-", new List<int> { 1, 2, 3 });
+", new List<int> { 1, 2, 3 }, null);
 
             var expectedResult = @"<span>1</span>
 <span>2</span>
