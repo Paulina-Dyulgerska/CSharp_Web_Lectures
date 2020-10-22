b011b89fb5b6fd5bcf62c2d8c90dffc9f8e3ce6d
Paulina-Dyulgerska <paylina_st@yahoo.com>
2020-10-22 17:07:46 +0300

    Database, migrations add.

--- Exam Prep/Apps/SharedTrip/Data/ApplicationDbContext.cs
+++ Exam Prep/Apps/SharedTrip/Data/ApplicationDbContext.cs
@@ -12,5 +12,25 @@
             : base(db)
         {
         }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Trip> Trips { get; set; }
+
+        public DbSet<UserTrip> UserTrips { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=SharedTrip;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserTrip>()
+                .HasKey(x => new { x.UserId, x.TripId });
+        }
     }
 }
---
+++ Exam Prep/Apps/SharedTrip/Data/Trip.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SharedTrip.Data
+{
+    public class Trip
+    {
+        public Trip()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.UserTrips = new HashSet<UserTrip>();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        public string StartPoint { get; set; }
+
+        [Required]
+        public string EndPoint { get; set; }
+
+        //Has a DepartureTime – a datetime(required) (use format: "dd.MM.yyyy HH:mm")
+        public DateTime DepartureTime { get; set; }
+
+        public byte Seats { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public virtual IEnumerable<UserTrip> UserTrips { get; set; }
+
+    }
+}
